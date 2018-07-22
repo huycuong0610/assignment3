bd9921a44e28d4ccba0e476c62417bf157140f22
huycuong0610 <huycuong0610@gmail.com>
2018-07-22 23:58:34 +0700

    Update record

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="menu">
     <h1>Accessible Drop Down Menu</h1>
+    <p><button tabindex="0" @click="startRecord()">Record</button></p>
+    <p
+      class="script"
+      :style="{display: results && results.length ? 'block' : 'none'}">
+      {{results}}
+    </p>
     <nav role="navigation">
         <ul class="access-menu">
             <li v-for="(value, key) in menu" :key="key">
@@ -15,10 +21,12 @@
 </template>
 
 <script>
+
 export default {
   name: 'Menu',
   data () {
     return {
+      results: '',
       menu: {
         Womens: {
           childs: [
@@ -60,9 +68,73 @@ export default {
               ],
               key: 4
         }
-      }
+      },
+      
     }
-  }
+  },
+  mounted() {
+          var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+          var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+          var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+let speechList = {
+      functions: ['open'],
+      methods: ['link'],
+      numbers: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    }
+    let grammarNumbers = ''
+    for (let i = 0; i < speechList.numbers.length; ++i) {
+      grammarNumbers += `${i === 0 ? '' : ' '}(${i} | ${speechList.numbers[i]})`
+    }
+    let grammar = `#JSGF V1.0;
+                    grammar menu;
+                    <functions> = ${speechList.functions.join(' | ')} ;
+                    <methods> = ${speechList.methods.join(' | ')} ;
+                    <numbs> = ${grammarNumbers} ;
+                    public <command> = <functions> <methods> number <numbs> `;
+          if (SpeechRecognition) {
+                let speechRecognitionList = new SpeechGrammarList()
+                speechRecognitionList.addFromString(grammar, 1)
+                this.recognition = new SpeechRecognition()
+                this.recognition.grammars = speechRecognitionList
+                this.recognition.lang = 'en-US'
+                this.recognition.interimResults = true
+                this.recognition.maxAlternatives = 1
+                this.recognition.onstart = () => {
+                    this.results = 'Recording'
+                    this.synth('Recording')
+                }
+                this.recognition.onspeechend = () => {
+                    this.results = ''
+                    this.recognition.stop()
+                }
+
+                this.recognition.onend = () => {
+                    
+                }
+                this.recognition.onresult = e => {
+                    result = e.results[0][0]['transcript']
+                    this.results = result
+                }
+            }
+      },
+    methods: {
+        startRecord () {
+        try {
+            this.recognition.start()
+        } catch (e) {
+            let text = 'Speech recognition stopped.'
+            this.recognition.stop()
+            this.results = text
+            this.synth(text)
+        }
+        },
+        synth (text = '') {
+            let synth = window.speechSynthesis
+            if (synth && text.length) {
+                synth.speak(new SpeechSynthesisUtterance(text))
+            }
+        },
+      }
 }
 </script>
 
